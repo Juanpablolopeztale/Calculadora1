22beb7c1376718d1a09ba305a34221e18a483540
Pablo <jplopez@scl.edu.gt>
2023-06-05 12:47:29 -0600

    Historial

--- js/index.js
+++ js/index.js
@@ -1,76 +1,55 @@
-const numbers = document.querySelectorAll('.numbers');
-const result = document.querySelector('.result span')
-const signs = document.querySelectorAll(".sign");
-const equals = document.querySelector(".equals");
-const clear = document.querySelector(".clear");
-const negative = document.querySelector(".negative");
-const percent = document.querySelector(".percent");
 
-let firstValue = "";
-let isFirstValue = false;
-let secondValue = "";
-let isSecondValue = false
-let sign = "";
-let resultValue = 0;
+const resultElement = document.querySelector('.result span');
+const clearButton = document.querySelector('.clear');
+const equalsButton = document.querySelector('.equals');
+const historyList = document.querySelector('#history-list');
 
-for(let i = 0; i < numbers.length; i++) {
-    numbers[i].addEventListener('click', (e) => {
-        let atr = e.target.getAttribute('value');
-        if(isFirstValue === false) {
-            getFirstValue(atr)
-        }
-        if(isSecondValue == false) {
-            getSecondValue(atr);
-        }
-    })
+// Variable para almacenar el historial
+let history = [];
+
+// Función para actualizar el historial
+function updateHistory(operation, result) {
+  history.push({ operation, result });
+  const listItem = document.createElement('li');
+  listItem.textContent = `${operation} = ${result}`;
+  historyList.appendChild(listItem);
 }
 
-function getFirstValue(el) {
-    result.innerHTML = "";
-    firstValue += el;
-    result.innerHTML = firstValue;
-    firstValue = +firstValue;
+
+function clearHistory() {
+  history = [];
+  historyList.innerHTML = '';
 }
-function getSecondValue(el) {
-    if(firstValue != "" && sign !="") {
-        secondValue += el;
-        result.innerHTML = secondValue;
-        secondValue = +secondValue;
+
+function evaluateOperation() {
+  const operation = resultElement.textContent;
+  try {
+    const result = eval(operation); 
+    resultElement.textContent = result;
+    updateHistory(operation, result);
+  } catch (error) {
+    resultElement.textContent = 'Error';
+  }
+}
+
+
+clearButton.addEventListener('click', () => {
+  resultElement.textContent = '0';
+  clearHistory();
+});
+
+
+equalsButton.addEventListener('click', evaluateOperation);
+
+const buttons = document.querySelectorAll('.numbers, .sign, .comma');
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const value = button.getAttribute('value');
+    const currentValue = resultElement.textContent;
+    if (currentValue === '0') {
+      resultElement.textContent = value;
+    } else {
+      resultElement.textContent += value;
     }
-}
-
-function getSign() {
-    for(let i = 0; i < signs.length; i++) {
-        signs[i].addEventListener('click', (e) => {
-            sign = e.target.getAttribute('value');
-            isFirstValue = true;
-        })
-    }
-}
-getSign();
-
-equals.addEventListener('click', () => {
-    result.innerHTML = "";
-    if(sign === "+" ) {
-        resultValue = firstValue + secondValue;
-    } else if(sign === "-" ) {
-        resultValue = firstValue - secondValue;
-    }else if(sign === "x" ) {
-        resultValue = firstValue * secondValue;
-    }else if(sign === "/" ) {
-        resultValue = firstValue / secondValue;
-    }
-    result.innerHTML = resultValue;
-    firstValue = resultValue;
-    secondValue = "";
-
-})
-function checkResultLenght() {
-    resultValue = JSON.stringify(resultValue)
-    
-    if(resultValue.length >= 8) {
-        resultValue = JSON.parse(resultValue);
-        result.innerHTML = resultValue.toFixed(5);
-
-    }
-}
+  });
+});
